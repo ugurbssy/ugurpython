15713d8be10180daa16112050f370c10d66a68bb
ugurbssy <62008595+ugurbssy@users.noreply.github.com>
2020-11-26 00:12:29 +0100

    Add files via upload

---
+++ space occupied.py
@@ -0,0 +1,23 @@
+import os
+
+#directory = ('C:/Users/ASUS/Desktop/courses - SGH/3rd Semester/Python')
+#directory =("C:\\Users\ASUS\Desktop/courses - SGH")
+
+def space_occupied(dir):
+
+    total = 0
+    try:
+        print("to add the size of", dir)
+
+        for link in os.scandir(dir):
+            if link.is_file():
+                total = total + link.stat().st_size
+            elif link.is_dir():
+                total = total + space_occupied(link)
+    except NotADirectoryError:
+        return os.path.getsize(dir)
+
+    return total
+
+print(space_occupied("C:\\Users\ASUS\Desktop\Luis"))
+print(space_occupied("C:\\Users\ASUS\Desktop\wt.txt"))
